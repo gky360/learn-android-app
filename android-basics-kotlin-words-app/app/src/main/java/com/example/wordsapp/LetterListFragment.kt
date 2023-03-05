bb3c53de926107db6c990b0871616b23ef393f91
gky360 <gky360@gmail.com>
2023-03-05 20:06:05 +0900

    Preferences DataStore

--- android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -24,15 +24,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.data.SettingsDataStore
 import com.example.wordsapp.databinding.FragmentLetterListBinding
+import kotlinx.coroutines.launch
 
 /**
  * Entry fragment for the app. Displays a [RecyclerView] of letters.
  */
 class LetterListFragment : Fragment() {
+    private lateinit var SettingsDataStore: SettingsDataStore
+
     private var _binding: FragmentLetterListBinding? = null
 
     // This property is only valid between onCreateView and
@@ -40,6 +46,7 @@ class LetterListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
+
     // Keeps track of which LayoutManager is in use for the [RecyclerView]
     private var isLinearLayoutManager = true
 
@@ -61,9 +68,16 @@ class LetterListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
-        // Sets the LayoutManager of the recyclerview
-        // On the first run of the app, it will be LinearLayoutManager
-        chooseLayout()
+
+        SettingsDataStore = SettingsDataStore(requireContext())
+        SettingsDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner) { value ->
+            isLinearLayoutManager = value
+            // Sets the LayoutManager of the recyclerview
+            // On the first run of the app, it will be LinearLayoutManager
+            chooseLayout()
+
+            activity?.invalidateOptionsMenu()
+        }
     }
 
     /**
@@ -117,6 +131,13 @@ class LetterListFragment : Fragment() {
                 // Sets layout and icon
                 chooseLayout()
                 setIcon(item)
+
+                lifecycleScope.launch {
+                    SettingsDataStore.saveLayoutToPreferencesStore(
+                        isLinearLayoutManager,
+                        requireContext()
+                    )
+                }
 
                 return true
             }
