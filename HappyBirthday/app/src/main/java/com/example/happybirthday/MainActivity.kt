628f2a20b564755de2d13d83d028859bd4201968
gky360 <gky360@gmail.com>
2023-03-06 08:35:49 +0900

    Practice: Compose Basics

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,9 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,12 +35,163 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HappyBirthdayTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    BirthdayGreetingWithImage("Happy Birthday Sam!", "- from Emma")
+                ComposeQuadrantScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrantScreen() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.weight(1f)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    QuadrantCard(
+                        title = stringResource(R.string.first_title),
+                        description = stringResource(R.string.first_description),
+                        backgroundColor = Color.Green
+                    )
+                }
+                Column(modifier = Modifier.weight(1f)) {
+                    QuadrantCard(
+                        title = stringResource(R.string.second_title),
+                        description = stringResource(R.string.second_description),
+                        backgroundColor = Color.Yellow
+                    )
+                }
+            }
+            Row(modifier = Modifier.weight(1f)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    QuadrantCard(
+                        title = stringResource(R.string.third_title),
+                        description = stringResource(R.string.third_description),
+                        backgroundColor = Color.Cyan
+                    )
+                }
+                Column(modifier = Modifier.weight(1f)) {
+                    QuadrantCard(
+                        title = stringResource(R.string.fourth_title),
+                        description = stringResource(R.string.fourth_description),
+                        backgroundColor = Color.LightGray
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun QuadrantCard(title: String, description: String, backgroundColor: Color) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(text = description, textAlign = TextAlign.Justify)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantPreview() {
+    HappyBirthdayTheme {
+        ComposeQuadrantScreen()
+    }
+}
+
+// ----------------------------------------------------------------
+
+@Composable
+fun TaskCompletedScreen() {
+    val icon = painterResource(id = R.drawable.ic_task_completed)
+    Surface {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            Image(painter = icon, contentDescription = null)
+            Text(
+                text = stringResource(R.string.all_tasks_completed),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+            )
+            Text(
+                text = stringResource(R.string.nice_work),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskCompletedPreview() {
+    HappyBirthdayTheme {
+        TaskCompletedScreen()
+    }
+}
+
+
+// ----------------------------------------------------------------
+
+@Composable
+fun Article() {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column {
+                Text(
+                    text = stringResource(R.string.article_title),
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.article_summary),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.article_text),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = false)
+@Composable
+fun ArticlePreview() {
+    HappyBirthdayTheme {
+        Article()
+    }
+}
+
+// ----------------------------------------------------------------
+
+@Composable
+fun Birthday() {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colorScheme.background) {
+        BirthdayGreetingWithImage("Happy Birthday Sam!", "- from Emma")
     }
 }
 
