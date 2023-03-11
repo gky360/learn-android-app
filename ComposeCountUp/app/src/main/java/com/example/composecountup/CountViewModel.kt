e043774ff0f179fcf994f1044c2420459221b25a
gky360 <gky360@gmail.com>
2023-03-11 15:54:48 +0900

    Add ViewModel

---
+++ ComposeCountUp/app/src/main/java/com/example/composecountup/CountViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.composecountup
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel() {
+    val count: MutableState<Int> = mutableStateOf<Int>(0)
+
+    fun onCountUpTapped() {
+        val currentValue = requireNotNull(count.value)
+        count.value = currentValue + 1
+    }
+}
--- ComposeCountUp/app/src/main/java/com/example/composecountup/MainActivity.kt
+++ ComposeCountUp/app/src/main/java/com/example/composecountup/MainActivity.kt
@@ -3,31 +3,31 @@ package com.example.composecountup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: CountViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CountUp()
+            CountUp(viewModel = viewModel)
         }
     }
 }
 
 @Composable
-fun CountUp() {
-    var count: Int by remember { mutableStateOf(0) }
+fun CountUp(viewModel: CountViewModel) {
+    val count: Int by viewModel.count
 
     Column {
         Text(text = "$count")
-        Button(onClick = { count += 1 }) {
+        Button(onClick = { viewModel.onCountUpTapped() }) {
             Text(text = "Count up!")
         }
     }
