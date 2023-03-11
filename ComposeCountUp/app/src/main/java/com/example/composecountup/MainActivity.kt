8a2b6f4ed14f7dcce26f955bdb7d791079abc3d0
gky360 <gky360@gmail.com>
2023-03-11 15:38:46 +0900

    Add composable functions

--- ComposeCountUp/app/src/main/java/com/example/composecountup/MainActivity.kt
+++ ComposeCountUp/app/src/main/java/com/example/composecountup/MainActivity.kt
@@ -3,41 +3,32 @@ package com.example.composecountup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composecountup.ui.theme.ComposeCountUpTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCountUpTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            CountUp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+fun CountUp() {
+    var count: Int by remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCountUpTheme {
-        Greeting("Android")
+    Column {
+        Text(text = "$count")
+        Button(onClick = { count += 1 }) {
+            Text(text = "Count up!")
+        }
     }
 }
